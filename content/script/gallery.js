9d97734531c71ae19d55ac0f40eb774365cdf7e6
rustammesquita <rustammesquita@users.noreply.github.com>
2021-06-19 04:38:14 -0300

    teste em cell

--- content/script/gallery.js
+++ content/script/gallery.js
@@ -328,11 +328,14 @@ function InsertPhoto(i)
   var slideshow = document.getElementsByClassName("slideshow")[0];
   slideshow.ontouchstart = function myFunction(event) {
     mouse_x_begin = event.touches[0].clientX;
+    alert(mouse_x_begin);
   }
   slideshow.ontouchmove = function myFunction(event) {
     mouse_x_end = event.touches[0].clientX;
+    alert(mouse_x_end);
   }
   slideshow.ontouchend = function myFunction(event) {
+    alert(mouse_x_begin - mouse_x_end);
     if (mouse_x_begin - mouse_x_end > 0)
       NextPhoto();
     else if (mouse_x_begin - mouse_x_end < 0)
