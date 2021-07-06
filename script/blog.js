cd8b5d9e1cffe5bb0058723f38d91b5b402ba0f4
rustammesquita <rustammesquita@users.noreply.github.com>
2021-07-06 14:25:06 -0300

    Completes blog waiting page and minor adjustments on colors

--- script/blog.js
+++ script/blog.js
@@ -10,6 +10,10 @@ function GetBlogMsg()
 {
   if (sessionStorage.getItem("lang") == pt_lang)
   {
-    return "Em breve, textos do blog <a href='https://semduvidassemcertezas.blogspot.com'>Sem D\&uacute;vidas, Sem Certezas</a> aqui."
+    return "Em breve, textos do blog <a class='blogLink' href='https://semduvidassemcertezas.blogspot.com'>Sem D\&uacute;vidas, Sem Certezas</a> aqui."
+  }
+  else
+  {
+    return "Texts of my blog <a class='blogLink' href='https://semduvidassemcertezas.blogspot.com'>Sem D\&uacute;vidas, Sem Certezas</a>, here soon."
   }
 }
