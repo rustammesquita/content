2ac065ae844bf992138d4943334811f72a3ca463
rustammesquita <rustammesquita@users.noreply.github.com>
2021-07-12 22:47:27 -0300

    Final test

--- form.php
+++ form.php
@@ -2,12 +2,13 @@
   $name = $_POST['name'];
   $email = $_POST['email'];
   $msg = $_POST['msg'];
+  $content = "User Name: $name.\nUser Email: $email.\nUser Message: $msg.\n";
   
-  $from = 'site@rustammesquita.com';
+  $from = 'contato@rustammesquita.com';
   $to = 'contato@rustammesquita.com';
   $subject = 'Site Message';
   $headers = "From: $from \r\n";
   $headers .= "Reply-To: $email \r\n";
   
-  mail($to, $subject, $msg, $headers);
+  mail($to, $subject, $content, $headers);
 ?>
