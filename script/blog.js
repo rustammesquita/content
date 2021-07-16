5236365f6726eccc3aeb4094a9cd4cec114232a7
rustammesquita <rustammesquita@users.noreply.github.com>
2021-07-16 17:00:00 -0300

    Local signature font and new guide to blog

--- script/blog.js
+++ script/blog.js
@@ -10,10 +10,10 @@ function GetBlogMsg ()
 {
   if (sessionStorage.getItem("lang") == pt_lang)
   {
-    return "Em breve, textos do blog <a class='blogLink' href='https://semduvidassemcertezas.blogspot.com'>Sem D\&uacute;vidas, Sem Certezas</a> aqui."
+    return "Em breve, textos do blog <a target='_blank' rel='noopener noreferrer' class='blogLink' href='https://semduvidassemcertezas.blogspot.com'>Sem D\&uacute;vidas, Sem Certezas</a> aqui."
   }
   else
   {
-    return "Texts of my blog <a class='blogLink' href='https://semduvidassemcertezas.blogspot.com'>Sem D\&uacute;vidas, Sem Certezas</a>, here soon."
+    return "Texts of my blog <a target='_blank' rel='noopener noreferrer' class='blogLink' href='https://semduvidassemcertezas.blogspot.com'>Sem D\&uacute;vidas, Sem Certezas</a>, here soon."
   }
 }
